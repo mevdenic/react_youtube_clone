9cbe482fcb0131989cc0f4b36cda0d7f8c49e8a6
mevdenic <mevdenic@gmail.com>
2024-02-11 22:27:21 +0100

    bug resolve on link tag

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Logo() {
     return (
-        <Link to="/">
+        <Link to="/" style={{ display: "contents" }}>
             <img src="/logo.png" alt="logo" height="100%" />
         </Link>
     );
